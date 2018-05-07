ce107a9ea7cac526d8f1273b30dd079d36ac56ee
Bryanv6 <vill.bryan72@gmail.com>
2018-05-07 15:08:00 -0400

    hibernate

---
+++ src/main/java/model/Batches.java
@@ -0,0 +1,22 @@
+package model;
+
+
+import javax.persistence.Entity;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+@Entity
+@Table(name="Batches")
+public class Batches {
+
+
+}
