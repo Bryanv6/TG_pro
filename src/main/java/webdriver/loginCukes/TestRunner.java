87adc8f058f3351beb56d1faa18ab406365a4453
Joey-Ni <joey.inmars@gmail.com>
2018-05-10 13:03:33 -0400

    test runner package

--- src/main/java/webdriver/loginCukes/TestRunner.java
+++ src/main/java/webdriver/loginCukes/TestRunner.java
@@ -1,7 +1,5 @@
 package webdriver.loginCukes;
 
-import org.junit.runner.RunWith;
-
 import cucumber.api.CucumberOptions;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
 @CucumberOptions(features="\\my_git_repos\\TG_pro\\src\\main\\resources\\loginFunction.feature")
