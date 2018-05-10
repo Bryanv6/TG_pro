6b003ec1ee3000a2707e64f036f2e1e69ab53f65
Bryanv6 <vill.bryan72@gmail.com>
2018-05-10 14:07:49 -0400

    found button

--- src/main/java/webdriver/overviewTest.java
+++ src/main/java/webdriver/overviewTest.java
@@ -1,8 +1,11 @@
 package webdriver;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
 
 import java.io.File;
@@ -16,7 +19,7 @@ public class overviewTest {
         d = webdriver.openApp();
         webdriver.trainerLogin();
         String name = d.findElement(By.xpath("//*[@id=\"view\"]/div/md-card/md-content/md-table-container/table")).getText();
-        
+
         System.out.println(name);
     }
 
@@ -24,7 +27,12 @@ public class overviewTest {
     public void overview_test(){
         d = webdriver.openApp();
         webdriver.trainerLogin();
+        d.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
         String name = d.findElement(By.xpath("//*[@id=\"view\"]/div/md-card/md-content/md-table-container/table")).getText();
+        //d.findElement(By.xpath("//img[@src='web/L001/images/arrow-up.svg']")).click();
+        WebElement mapObject = d.findElement(By.xpath("//*[name()='svg']/*[name()='path=M4 12l1.41 ']"));
+        Actions builder = new Actions(d);
+        builder.click(mapObject).build().perform();
         System.out.println(name);
     }
 }
