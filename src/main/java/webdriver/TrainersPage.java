ae0ee70246a2f31ac4b3870925960f49e21568bb
Bryanv6 <vill.bryan72@gmail.com>
2018-05-11 11:00:04 -0400

    trainer test

--- src/main/java/webdriver/TrainersPage.java
+++ src/main/java/webdriver/TrainersPage.java
@@ -8,17 +8,50 @@ import org.openqa.selenium.WebElement;
  * Created by bryanvillegas on 5/10/18.
  */
 public class TrainersPage {
-    private static WebElement element;
 
     public static WebElement trainersBtn(WebDriver wd){
-        element = wd.findElement(By.xpath("//li[@name='trainers']"));
-        return element;
+        return wd.findElement(By.xpath("//li[@name='trainers']"));
     }
+    //for VP only
     public static WebElement addTrainer(WebDriver wd){
         return wd.findElement(By.xpath("//*[@id=\"view\"]/md-card/md-toolbar[1]/div/button[1]"));
     }
-    public static WebElement inputName(WebDriver wd){
+    //for VP only
+    public static WebElement inputFirstname(WebDriver wd){
         return wd.findElement(By.id("input_1"));
     }
+    //for VP only
+    public static WebElement inputLastname(WebDriver wd){
+        return wd.findElement(By.id("input_903"));
+    }
+    //for VP only
+    public static WebElement submit(WebDriver wd){
+        return wd.findElement(By.xpath("/html/body/div[4]/md-dialog/form/md-dialog-actions/button[1]"));
+    }
+    //for VP only
+    public static WebElement cancel(WebDriver wd){
+        return wd.findElement(By.xpath("/html/body/div[4]/md-dialog/form/md-dialog-actions/button[2]"));
+    }
+    public static WebElement calenderBtn(WebDriver wd){
+        return wd.findElement(By.xpath("//*[@id=\"view\"]/md-card/md-toolbar[1]/div/button[2]"));
+    }
+    public static WebElement newPTO(WebDriver wd){
+        return wd.findElement(By.xpath("//*[@id=\"dialogContent_889\"]/md-dialog-actions/button[1]"));
+    }
+    //for VP only
+    public static WebElement deactivateTrainer(WebDriver wd){
+        return wd.findElement(By.xpath("//*[@id=\"view\"]/md-card/md-content[1]/md-list/md-list-item[1]/div[1]/div[1]/button[2]"));
+    }
+    //for VP only
+    public static WebElement reativateTrainer(WebDriver wd){
+        return wd.findElement(By.xpath("//*[@id=\"view\"]/md-card/md-content[2]/md-list/md-list-item[1]/button"));
+    }
+    public static WebElement goToProfile(WebDriver wd){
+        return wd.findElement(By.xpath("//*[@id=\"view\"]/md-card/md-content[1]/md-list/md-list-item[1]/div[1]/button"));
+    }
+    public static WebElement getInactive(WebDriver wd){
+        return wd.findElement(By.xpath("//*[@id=\"view\"]/md-card/md-content[2]"));
+    }
+
 
 }
--- src/main/java/webdriver/TrainersTest.java
+++ src/main/java/webdriver/TrainersTest.java
@@ -1,11 +1,16 @@
 package webdriver;
 
+import antlr.StringUtils;
+import bsh.StringUtil;
 import gherkin.lexer.Tr;
 import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -22,12 +27,30 @@ public class TrainersTest {
     }
     @AfterTest
     public void afterTest(){
-       // webdriver.logout();
+        webdriver.logout();
     }
     @Test
     public void test1(){
         d.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+
+
+    }
+    @Test
+    public void enterTrainerAndSubmit(){
         TrainersPage.addTrainer(d).click();
-        TrainersPage.inputName(d).sendKeys("Bryan");
+        TrainersPage.inputFirstname(d).sendKeys("Test");
+        TrainersPage.inputLastname(d).sendKeys("Name");
+        TrainersPage.submit(d).click();
+
+        String x = TrainersPage.getInactive(d).getText();
+        String lines[] = x.split("[\r\n]+");
+        for(String name : lines){
+            if(name.equals("Test Name")) {
+                System.out.println("Found Name");
+                Assert.assertTrue(name.equals("Test Name"));
+                break;
+            }
+        }
     }
 }
