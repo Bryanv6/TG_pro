bb4f2cccd77e300a53c8c48b4fa409421b883b5a
Katherine obioha <katobioha@gmail.com>
2018-05-16 09:53:31 -0400

    editing final push

--- src/test/java/com/gator/tests/BatchesTest.java
+++ src/test/java/com/gator/tests/BatchesTest.java
@@ -1,54 +1,101 @@
 package com.gator.tests;
+
 import org.testng.annotations.Test;
 
 
+
 import com.gator.pages.BatchesPage;
+
 import org.testng.annotations.BeforeTest;
+
 import org.testng.annotations.Listeners;
 
-import webdriver.DriverSingleton;
 import org.openqa.selenium.Dimension;
+
 import org.testng.Assert;
+
 import org.testng.annotations.AfterTest;
 
+import webdriver.DriverSingleton;
+
 import org.openqa.selenium.WebDriver;
+
 @Listeners(TestResultListener.class)
+
 public class BatchesTest {
+
 	static 	WebDriver d=DriverSingleton.getDriver();
+
 	 BatchesPage batches = new BatchesPage(d); 
+
 	 
+
 	
+
 	@Test
+
 	public void verifyPageLoad() {
+
 		BatchesPage.batchesBtn(d).click();
+
 		Assert.assertTrue(d.getCurrentUrl().contains("batches"));
+
 		
+
 	}
+
 	
+
 	@Test
+
 	public void verifyTableLoad() {
+
 		
+
 		Dimension before = batches.batch_card.getSize();
+
 		batches.batch_card.click();
+
 		Dimension after = batches.batch_card.getSize();
+
 		Assert.assertTrue(before.getHeight()<after.getHeight());	
+
 	}
+
 	
+
 	@Test
+
 	public void verifyTimelineLoad() {
+
 		Dimension dim = batches.batch_timeline.getSize();
+
 		Assert.assertTrue(dim.getHeight()>0);
+
 	}
+
 	
+
 	@Test
+
 	public void verifyDateSort() {
+
 		
+
 		
+
 	}
 
 
+
+
+
 	
+
 	
+
+
+
 
 
 }
--- src/test/java/com/gator/tests/CurriculumToggleCucumber.java
+++ src/test/java/com/gator/tests/CurriculumToggleCucumber.java
@@ -22,6 +22,30 @@ public class CurriculumToggleCucumber {
 	    d.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
 	    d.findElement(By.xpath("//li[@name='curricula']")).click();*/
 	}
+	
+	@Given("^I \"([^\"]*)\" on the core toggle$")
+	public void i_on_the_core_toggle(String arg1) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^a list of core \"([^\"]*)\" should not be displayed$")
+	public void a_list_of_core_should_not_be_displayed(String arg1) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Given("^I \"([^\"]*)\" on the focus toggle$")
+	public void i_on_the_focus_toggle(String arg1) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^a list of focus\"([^\"]*)\" should not be displayed$")
+	public void a_list_of_focus_should_not_be_displayed(String arg1) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
 
 
 
